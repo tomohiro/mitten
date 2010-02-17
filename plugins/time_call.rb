754a2c573b8729da581915313568668304261fbc
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-02-17 18:13:30 +0900

    add 時報

---
+++ plugins/time_call.rb
@@ -0,0 +1,26 @@
+require 'ostruct'
+require 'yaml'
+
+class TimeCall < Mint::Plugin
+  def initialize(config, socket)
+    super(config, socket)
+
+    @config_file = config['config']
+  end
+
+  def before_hook
+    @call_list = load_config
+  end
+
+  def main
+    now = "#{Time.now.hour}#{Time.now.min}".to_i
+    @channels.each do |channel|
+      notice(channel, @call_list[now]) if @call_list[now]
+    end
+  end
+
+  def load_config
+    @config = OpenStruct.new(File.open(@config_file) { |f| YAML.load(f) })
+    @config.time
+  end
+end
