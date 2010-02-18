9a4482887c8e2df62e48d642bf06116d12baad0c
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-02-18 17:09:23 +0900

    add Rspec describe case 'Mint::Core instantiation'

--- spec/mint_spec.rb
+++ spec/mint_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Mint" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before(:all) do
+    @mint = Mint::Core.new
+  end
+
+  it 'Mint::Core Instanctiation' do
+    @mint.class.should == Mint::Core
   end
 end
