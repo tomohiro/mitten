fa643ac823df87ebf737ceb4b8852ef30363cba7
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-02-19 14:51:45 +0900

    mixi ボイスからの投稿をわかりやすくした

--- plugins/mixi_voice.rb
+++ plugins/mixi_voice.rb
@@ -50,7 +50,7 @@ class MixiVoice < Mint::Plugin
     voices.sort.each do |key, voice|
       if @caches.empty? or !@caches.has_key? key
         @channels.each do |channel|
-          message(channel, "[#{voice[:nickname]}]#{voice[:reply]} #{voice[:comment]}")
+          message(channel, "mixi voice: [#{voice[:nickname]}]#{voice[:reply]} #{voice[:comment]}")
           sleep 5
         end
       end
