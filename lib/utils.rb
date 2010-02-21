578922a5506cff72f48e3f91b29750ac10313b70
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-02-21 20:18:45 +0900

    fixed j.mp method

--- lib/utils.rb
+++ lib/utils.rb
@@ -3,10 +3,18 @@ require 'open-uri'
 require 'json'
 
 module URI
-  def short(uri)
-    uri = URI.escape("http://j.mp/?s=&keyword=&url=#{target}")
-    Nokogiri::HTML(open(uri).read).at('#shortened-url')['value']
-  end
+  SHORT_URI_API_LOGIN = ENV['SHORT_URI_API_LOGIN']
+  SHORT_URI_API_TOKEN = ENV['SHORT_URI_API_TOKEN']
 
-  module_function :short
+  def self.short(uri)
+    return uri if SHORT_URI_API_LOGIN == nil
+    begin
+      query  = "http://api.j.mp/shorten?version=2.0.1&longUrl=#{uri.gsub('&', '%26')}&login=#{SHORT_URI_API_LOGIN}&apiKey=#{SHORT_URI_API_TOKEN}"
+      result = JSON.parse(open(query).read)
+
+      result.first[1].first[1]['shortUrl']
+    rescue Exception => e
+      URI.encode(uri)
+    end
+  end
 end
