48ea3b4ff0f24926b25910c50ea2d0ed6210c773
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-02-21 19:19:06 +0900

    URI short method is change

--- lib/utils.rb
+++ lib/utils.rb
@@ -4,17 +4,8 @@ require 'json'
 
 module URI
   def short(uri)
-    begin
-      login   = 'tomohiro'
-      api_key = 'R_9ed4fae410ab7b729a15dc91c8f7687c'
-
-      query  = "http://api.j.mp/shorten?version=2.0.1&longUrl=#{uri}&login=#{login}&apiKey=#{api_key}"
-      result = JSON.parse(open(query).read)
-
-      result.first[1].first[1]['shortUrl']
-    rescue
-      URI.encode(uri)
-    end
+    uri = URI.escape("http://j.mp/?s=&keyword=&url=#{target}")
+    Nokogiri::HTML(open(uri).read).at('#shortened-url')['value']
   end
 
   module_function :short
