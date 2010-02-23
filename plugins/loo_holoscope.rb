1f93e562c212b07a875e9bf3d7fc178bd8af88bd
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-02-23 19:30:28 +0900

    ルー語占いを追加

---
+++ plugins/loo_holoscope.rb
@@ -0,0 +1,51 @@
+require 'nkf'
+
+require 'open-uri'
+require 'nokogiri'
+
+class LooHoloscope < Mitten::Plugin
+  def initialize(*args)
+    super
+
+    @suffix = @config['suffix'] || 'の運勢教えて'
+    @signs = {
+      'おひつじ座' => 'http://lou5.jp/uranai/aries/',
+      'おうし座'   => 'http://lou5.jp/uranai/taurus/',
+      'ふたご座'   => 'http://lou5.jp/uranai/gemini/',
+      'かに座'     => 'http://lou5.jp/uranai/cancer/',
+      'しし座'     => 'http://lou5.jp/uranai/leo/',
+      'おとめ座'   => 'http://lou5.jp/uranai/virgo/',
+      'てんびん座' => 'http://lou5.jp/uranai/libra/',
+      'さそり座'   => 'http://lou5.jp/uranai/scorpio/',
+      'いて座'     => 'http://lou5.jp/uranai/sagittarius/',
+      'やぎ座'     => 'http://lou5.jp/uranai/capricorn/',
+      'みずがめ座' => 'http://lou5.jp/uranai/aquarius/',
+      'うお座'     => 'http://lou5.jp/uranai/pisces/',
+    }
+  end
+
+  def on_privmsg(prefix, channel, message)
+    case message
+    when /^(.+)#{@suffix}$/
+      result = get_result(@signs[$1])
+
+      if result == nil
+        notice(channel, '／(^o^)＼ わかにゃいっ') 
+      else 
+        notice(channel, result) 
+      end
+    end
+  end
+
+  private
+
+  def get_result(uri)
+    begin
+      doc = Nokogiri::HTML(open(uri).read)
+
+      loo = 'ルー占い: ' + (doc/'div.box-inner/p').first.text.gsub("\n", '')
+    rescue Exception => e
+      e.to_s
+    end
+  end
+end
